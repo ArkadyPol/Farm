2e897fb46082f80eb130f6b5eff0c505399c273e
Arkady Polhovsky <arkims@inbox.ru>
2020-10-24 16:47:15 +0300

    fix keyboard event

--- types/listeners/keyboard.ts
+++ types/listeners/keyboard.ts
@@ -2,26 +2,30 @@ import { onDragDown } from "./mouse.js";
 import { player, modes } from "../data.js";
 
 export function onKeyDown(e: KeyboardEvent) {
-  switch (e.key) {
-    case "w":
+  switch (e.code) {
+    case "ArrowUp":
+    case "KeyW":
       player.moveUp();
       break;
-    case "a":
+    case "ArrowLeft":
+    case "KeyA":
       player.moveLeft();
       break;
-    case "s":
+    case "ArrowDown":
+    case "KeyS":
       player.moveDown();
       break;
-    case "d":
+    case "ArrowRight":
+    case "KeyD":
       player.moveRight();
       break;
-    case "i":
+    case "KeyI":
       modes.isInventory = !modes.isInventory;
       if (modes.isInventory) {
         document.addEventListener("mousedown", onDragDown);
       } else document.removeEventListener("mousedown", onDragDown);
       break;
-    case "f":
+    case "KeyF":
       modes.isFPS = !modes.isFPS;
       break;
   }
