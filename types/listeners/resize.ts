ee77b1e0797cd57b85677349e6be1ccd206731c5
Arkady Polhovsky <arkims@inbox.ru>
2020-10-27 19:04:39 +0300

    fix resize bug

--- types/listeners/resize.ts
+++ types/listeners/resize.ts
@@ -1,4 +1,4 @@
-import { size, start } from "../data.js";
+import { center, size, start } from "../data.js";
 
 function onResize(e: UIEvent) {
   const canvas = document.querySelector("canvas");
@@ -6,6 +6,8 @@ function onResize(e: UIEvent) {
   size.height = document.documentElement.clientHeight;
   start.x = size.width - 300; // начальный Х для инвентаря
   start.y = size.height - 300; // начальный Y для инвентаря
+  center.x = size.width / 2;
+  center.y = size.height / 2;
   if (canvas) {
     canvas.width = size.width;
     canvas.height = size.height;
